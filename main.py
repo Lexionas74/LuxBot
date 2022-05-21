8484816ff9060241cb635d1076176de20614af45
Lexionas74 <72361814+Lexionas74@users.noreply.github.com>
2022-05-21 01:35:36 +0000

    yes

--- main.py
+++ main.py
@@ -5,16 +5,29 @@ from datetime import datetime
 from nextcord import Guild, Interaction, Message
 from nextcord.ext import commands
 
-bot = commands.Bot(command_prefix="s!")
+bot = commands.Bot(command_prefix="s!", intents=nextcord.Intents.all(), owner_ids ={935434855397871628, 687882857171255309})
 my_secret = os.environ['TOKEN']
 
 @bot.event
 async def on_ready():
+	print("3")
+	print("2")
+	print("1")
 	print("Blastoff!") # Space theme lol
+
+
 
 @bot.command()
 async def test(ctx):
 	await ctx.send("Test completed")
-
+	
+@bot.slash_command(name="hi", description="This is a test")
+async def hi(interaction: Interaction):
+	await interaction.response.send_message("HI")
+	
+@bot.slash_command(name="about", description="About the bot")
+async def about(interaction: Interaction):
+	embed = nextcord.Embed(title="About", description="LuxBot is a bot created by Lexionas#1535 and Luziaf#9464 for an event!\nHope you enjoy the bot!", color=nextcord.Colour.green())
+	await interaction.response.send_message(embed=embed)
   
-bot.run('TOKEN')
+bot.run(my_secret)
