122156a77ca3accbef27069046e031c84b4d6b96
Lexionas74 <72361814+Lexionas74@users.noreply.github.com>
2022-05-24 06:24:24 +0000

    a

--- main.py
+++ main.py
@@ -20,35 +20,28 @@ async def on_ready():
 	print("1")
 	print("Blastoff!") # Space theme lol
 
-@bot.command()
-async def test(ctx):
-	await ctx.send("Test completed")
 	
-@bot.slash_command(name="hi", description="This is a test")
-async def hi(interaction: Interaction):
-	await interaction.response.send_message("HI")
-	
-@bot.slash_command(name="about", description="About the bot")
+@bot.slash_command(name="about", description="About the bot") #this command shows a small bit of info about the bot
 async def about(interaction: Interaction):
 	embed = nextcord.Embed(title="About", description="LuxBot is a bot created by Lexionas#1535 and Luziaf#9464 for an event!\nHope you enjoy the bot!", color=nextcord.Colour.green())
 	await interaction.response.send_message(embed=embed)
 
-@bot.slash_command(name="ping", description="See the latency of the bot")
+@bot.slash_command(name="ping", description="See the latency of the bot") #this command shows the bots ping
 async def ping(interaction: Interaction):
 	ping = nextcord.Embed(title="Bot latency", description=f"`{round(bot.latency * 1000)}ms`", color=nextcord.Colour.green()) 
 	await interaction.response.send_message(embed=ping)
 
-@bot.slash_command(name="ban", description="Ban a member from the server")
+@bot.slash_command(name="ban", description="Ban a member from the server") #this command bans a member from the server
 async def ban_member(interaction: Interaction, member: nextcord.Member, reason="No reason provided"):
   await member.ban(reason=reason)
   await interaction.response.send_message(f"{member} has been exiled from the spaceship\nReason : {reason}")
 
-@bot.slash_command(name="kick", description="Kick a member from the server")
+@bot.slash_command(name="kick", description="Kick a member from the server") #this command kicks a member from the server
 async def kick_member(interaction: Interaction, member : nextcord.Member, reason="No reason provided"):
 	await member.kick(reason=reason)
 	await interaction.response.send_message(f"{member} was ejected from the spaceship\nReason : {reason}")
 
-@bot.slash_command(name="unban", description="Bring a member back from exile") #this 
+@bot.slash_command(name="unban", description="Bring a member back from exile") #this command unbans a user from the server
 async def unban(interaction: Interaction, user : nextcord.User):
 	await interaction.guild.unban(user=user)
 	await interaction.response.send_message(f"{user} was unexiled")
